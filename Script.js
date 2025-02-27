f4573ffd98adf9fc271f661399e3f82f96c966d2
FR4NCISCO1106 <francisquito1120@gmail.com>
2025-02-27 00:07:14 -0400

    feat:paginaweb

---
+++ Script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container")
+const btnSignIn = document.getElementById("btn-Sign-in")
+const btnSignUp = document.getElementById("btn-Sign-up")
+
+btnSignIn.addEventListener("click",()=>{
+  container.classList.remove("toggle");
+});
+btnSignUp.addEventListener("click",()=>{
+  container.classList.add("toggle");
+});
